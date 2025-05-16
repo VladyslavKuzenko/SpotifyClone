de75dbcc0e8b4e9c9605d8728e2227a7a83263cd
akka207 <alex2007kosh@gmail.com>
2025-05-16 11:33:25 +0200

    api init

---
+++ api/src/main/java/edu/itstep/api/controlers/UsersController.java
@@ -0,0 +1,54 @@
+package edu.itstep.api.controlers;
+
+import edu.itstep.api.models.User;
+import edu.itstep.api.repositories.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:3000")
+public class UsersController {
+
+    private final UserRepository userRepository;
+
+    public UsersController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    @PostMapping
+    public ResponseEntity createUser(@RequestBody User user) throws URISyntaxException {
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.created(new URI("/users/" + savedUser.getId())).body(savedUser);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateUser(@PathVariable Long id, @RequestBody User user) {
+        User currentUser = userRepository.findById(id).orElseThrow(RuntimeException::new);
+        currentUser.setName(user.getName());
+        currentUser.setEmail(user.getEmail());
+        currentUser = userRepository.save(user);
+
+        return ResponseEntity.ok(currentUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ api/src/test/java/edu/itstep/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.itstep.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
