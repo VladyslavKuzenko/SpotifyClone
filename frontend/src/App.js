734726c7ae89df96360be401684ef830ef0b40c0
VladyslavKuzenko <dalbayb281@gmail.com>
2025-06-08 19:12:14 +0200

    Removed the request for API
    Removed the request for API, it was test and now prevents to do front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,32 +3,11 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    const fetchClients = async () => {
-      const response = await fetch('http://localhost:8080/users');
-      const body = await response.json();
-      setUsers(body);
-    }
-
-    fetchClients();
-  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="App-intro">
-          <h2>Users</h2>
-          {users.map(client =>
-            <div key={client.id}>
-              {client.name} ({client.email})
-            </div>
-          )}
-        </div>
-      </header>
-    </div>
+   <div>
+      Working
+   </div>
   );
 }
 
