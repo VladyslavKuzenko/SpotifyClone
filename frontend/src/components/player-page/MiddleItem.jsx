4a8c969723ebd3fb1d274d3c82b4e3c7c75ada50
VladyslavKuzenko <dalbayb281@gmail.com>
2025-07-02 14:56:52 +0200

    added the reflection of the artist in the songs

--- frontend/src/components/player-page/MiddleItem.jsx
+++ frontend/src/components/player-page/MiddleItem.jsx
@@ -12,7 +12,7 @@ export default function MiddleItem({
   onSongSelect,
   onSetCurrentSongList,
   isPlaylistsChangesControl,
-  /*  artist, */
+
   /*   artistControl, */
 }) {
   const [search, setSearch] = useState("");
