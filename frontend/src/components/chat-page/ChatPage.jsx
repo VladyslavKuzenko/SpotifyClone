9f720f9c8e280ea0a13cdf0de1a0571df8e9e321
B1mba <partytopmaker@gmail.com>
2025-07-04 19:35:52 +0300

    Update active button style and adjust layout width
    Renamed the .active CSS class to .active1 and updated all related references in LeftSide.jsx for button highlighting. Also adjusted the .middle class width from 50% to 45% for improved layout.

--- frontend/src/components/chat-page/ChatPage.jsx
+++ frontend/src/components/chat-page/ChatPage.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import styles from "./ChatPage.module.css";
-import ChatList from './ChatList'
-import MyMessageItem from './MyMessageItem'
-import UserMessageItem from './UserMessageItem'
-import Footer from './Footer'
+import ChatList from './ChatList';
+import MyMessageItem from './MyMessageItem';
+import UserMessageItem from './UserMessageItem';
+import Footer from './Footer';
+import LeftSide from '../main-components/LeftSide';
+
 import UpperContent from "./UpperContent";
 const ChatPage = () => {
     return (
+
         <div className={styles.container}>
+
             <div className={styles["right-side"]}>
+
                 <ChatList />
 
                 <div className={styles["chat-messages"]}>
