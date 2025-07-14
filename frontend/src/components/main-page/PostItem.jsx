ae8c6b1b3c5918fb48d3214bf8ead4e65af33039
VladyslavKuzenko <dalbayb281@gmail.com>
2025-07-14 21:15:13 +0200

    Add image slider and improved post display to PostItem
    Introduced an image slider for posts with multiple images, including navigation buttons and dot indicators. Refactored post rendering to show only a limited number of posts with a 'Show more' button, and improved the comment input area. Also cleaned up and reorganized the component for better readability and maintainability.

--- frontend/src/components/main-page/PostItem.jsx
+++ frontend/src/components/main-page/PostItem.jsx
@@ -1,10 +1,165 @@
+// import React, { useEffect, useState } from "react";
+// import styles from "./main.module.css";
+// import { useAPI } from "../../hooks/useApi";
+// import { useAuth0 } from "@auth0/auth0-react";
+// import { isLiked } from "../../js/functions/functions";
+
+// export default function PostItem({ selectedTab }) {
+//   const [posts, setPosts] = useState([]);
+//   const { apiFetch } = useAPI();
+//   const { user, isLoading } = useAuth0();
+
+//   const [pomhIsModalOpen, setPomhIsModalOpen] = useState(false);
+//   const [pomhModalImageUrl, setPomhModalImageUrl] = useState("");
+
+//   const openPomhModal = (url) => {
+//     setPomhModalImageUrl(url);
+//     setPomhIsModalOpen(true);
+//   };
+
+//   const closePomhModal = () => {
+//     setPomhIsModalOpen(false);
+//     setPomhModalImageUrl("");
+//   };
+
+//   const fetchPosts = async () => {
+//     if(isLoading) return;
+//     var response;
+//     // console.log("FETCH POST. SELECTED TAB: ",selectedTab)
+//     // console.log("FETCH POST. user sub: ",user.sub)
+
+
+//     if (selectedTab==="artists") response = await apiFetch("/posts");
+//     else if (selectedTab==="friends") {
+//       response = await apiFetch(`/posts/byFollowing/${user.sub}`);
+//     } else {
+//       response = await apiFetch("/posts");
+//     }
+
+//     const data = await response.json();
+
+//     setPosts(data);
+//   };
+
+//   const submiteUserLike = async (post) => {
+//     const isPostLiked = await isLiked(post, user, apiFetch);
+//     const response = await apiFetch(`/users/like/${post.id}/${user.sub}`, {
+//       method: isPostLiked ? "DELETE" : "POST",
+//     });
+//     if (response.ok) {
+//       fetchPosts();
+//     } else {
+//       console.error("Failed to like/unlike the post");
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchPosts();
+//   }, [selectedTab,isLoading]);
+
+//   if (isLoading) return <div>Loading...</div>;
+
+//   return (
+//     <>
+//       {/* {friends&&<div>TEEEEEEEEEEEST</div>} */}
+//       {posts?.map((post) => (
+//         <div className={styles.main} key={post.id}>
+//           <div className={styles["post-item"]}>
+//             <div className={styles["post-content"]}>
+//               <div className={styles["upper-content"]}>
+//                 <div className={styles["post-ava-plat"]}>
+//                   <div className={styles["post-ava"]}></div>
+//                 </div>
+//                 <div className={styles["name-time"]}>
+//                   <div className={styles["post-author"]}>
+//                     {post.user.username}
+//                   </div>
+//                   <div className={styles["post-time"]}>10 days ago</div>
+//                 </div>
+//                 <div className={styles["like-coment-repost"]}>
+//                   <div
+//                     className={
+//                       isLiked(post, user, apiFetch)
+//                         ? styles["post-like-btn-active"]
+//                         : styles["post-like"]
+//                     }
+//                     onClick={() => submiteUserLike(post)}
+//                   >
+//                     <div className={styles["post-wrap"]}>
+//                       <button className={styles["post-like-btn"]}></button>
+//                       <div className={styles["likes-count"]}>
+//                         {post.likesCount}
+//                       </div>
+//                     </div>
+//                   </div>
+
+//                   <div className={styles["post-coment"]}>
+//                     <div className={styles["post-wrap"]}>
+//                       <button className={styles["post-coment-btn"]}></button>
+//                       <div className={styles["coment-count"]}>
+//                         {post.commentsCount}
+//                       </div>
+//                     </div>
+//                   </div>
+
+//                   <div className={styles["post-repost"]}>
+//                     <div className={styles["post-wrap"]}>
+//                       <button className={styles["post-repost-btn"]}></button>
+//                       <div className={styles["repost-count"]}>
+//                         {post.repostsCount}
+//                       </div>
+//                     </div>
+//                   </div>
+//                 </div>
+//               </div>
+
+//               <div className={styles["middle-content"]}>
+//                 <div className={styles["text-content"]}>{post.description}</div>
+//               </div>
+
+//               <div className={styles["post-photos-container"]}>
+//                 {post.contents?.map((content) => (
+//                   <div className={styles["post-photo"]} key={content.id}>
+//                     <img
+//                       className={styles["preview-image"]}
+//                       src={content.mediaUrl}
+//                       alt="Post content"
+//                       onClick={() => openPomhModal(content.mediaUrl)}
+//                     />
+//                   </div>
+//                 ))}
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       ))}
+
+//       {/* POMH MODAL */}
+//       {pomhIsModalOpen && (
+//         <div className={styles["pomh-modal-overlay"]} onClick={closePomhModal}>
+//           <div
+//             className={styles["pomh-modal-content"]}
+//             onClick={(e) => e.stopPropagation()}
+//           >
+//             <img
+//               src={pomhModalImageUrl}
+//               alt="Preview"
+//               className={styles["pomh-modal-image"]}
+//             />
+//           </div>
+//         </div>
+//       )}
+//     </>
+//   );
+// }
+
+
 import React, { useEffect, useState } from "react";
 import styles from "./main.module.css";
 import { useAPI } from "../../hooks/useApi";
 import { useAuth0 } from "@auth0/auth0-react";
 import { isLiked } from "../../js/functions/functions";
 import ChatMessage from "./ChatMessage";
-
 export default function PostItem({ selectedTab }) {
   const [posts, setPosts] = useState([]);
   const { apiFetch } = useAPI();
@@ -30,7 +185,7 @@ export default function PostItem({ selectedTab }) {
   };
 
   const fetchPosts = async () => {
-    if(isLoading) return;
+ if(isLoading) return;
     var response;
     // console.log("FETCH POST. SELECTED TAB: ",selectedTab)
     // console.log("FETCH POST. user sub: ",user.sub)
@@ -46,6 +201,12 @@ export default function PostItem({ selectedTab }) {
     const data = await response.json();
 
     setPosts(data);
+    
+    const initialIndexes = {};
+    data.forEach((post) => {
+      initialIndexes[post.id] = 0;
+    });
+    setCurrentImageIndexes(initialIndexes);
   };
 
   const submiteUserLike = async (post) => {
@@ -60,77 +221,166 @@ export default function PostItem({ selectedTab }) {
     }
   };
 
+  const changeImage = (postId, direction, total) => {
+    setCurrentImageIndexes((prev) => {
+      const currentIndex = prev[postId] || 0;
+      const nextIndex =
+        direction === "next"
+          ? Math.min(currentIndex + 1, total - 1)
+          : Math.max(currentIndex - 1, 0);
+      return { ...prev, [postId]: nextIndex };
+    });
+  };
+
+  const handleShowMore = () => {
+    setVisibleCount((prev) => prev + 15);
+  };
+
   useEffect(() => {
     fetchPosts();
   }, [selectedTab,isLoading]);
 
   if (isLoading) return <div>Loading...</div>;
 
-  const totalPosts = [...posts];
-  const start = Math.max(totalPosts.length - visibleCount, 0);
-  const visiblePosts = totalPosts.slice(start);
+ 
+  const start = Math.max(posts.length - visibleCount, 0);
+  const visiblePosts = posts.slice(start);
   const reversedVisiblePosts = [...visiblePosts].reverse();
 
   return (
     <>
-      {/* {friends&&<div>TEEEEEEEEEEEST</div>} */}
-      {posts?.map((post) => (
-        <div className={styles.main} key={post.id}>
-          <div className={styles["post-item"]}>
-            <div className={styles["post-content"]}>
-              <div className={styles["upper-content"]}>
-                <div className={styles["post-ava-plat"]}>
-                  <div className={styles["post-ava"]}></div>
-                </div>
-                <div className={styles["name-time"]}>
-                  <div className={styles["post-author"]}>
-                    {post.user.username}
+      <div className={styles["posts-container"]}>
+        {reversedVisiblePosts.map((post) => {
+          const currentImageIndex = currentImageIndexes[post.id] || 0;
+          const images = post.contents || [];
+          const hasDescription = post.description && post.description.trim() !== "";
+
+          return (
+            <div className={styles.main} key={post.id}>
+              <div className={styles["post-item"]}>
+                <div className={styles["post-content"]}>
+                  <div className={styles["upper-content"]}>
+                    <div className={styles["post-ava-plat"]}>
+                      <div className={styles["post-ava"]}></div>
+                    </div>
+                    <div className={styles["name-time"]}>
+                      <div className={styles["post-author"]}>
+                        {post.user.username}
+                      </div>
+                      <div className={styles["post-time"]}>10 days ago</div>
+                    </div>
+                    <div className={styles["like-coment-repost"]}>
+                      <div
+                        className={
+                          isLiked(post, user, apiFetch)
+                            ? styles["post-like-btn-active"]
+                            : styles["post-like"]
+                        }
+                        onClick={() => submiteUserLike(post)}
+                      >
+                        <div className={styles["post-wrap"]}>
+                          <button className={styles["post-like-btn"]}></button>
+                          <div className={styles["likes-count"]}>
+                            {post.likesCount}
+                          </div>
+                        </div>
+                      </div>
+                      <div className={styles["post-coment"]}>
+                        <div className={styles["post-wrap"]}>
+                          <button className={styles["post-coment-btn"]}></button>
+                          <div className={styles["coment-count"]}>
+                            {post.commentsCount}
+                          </div>
+                        </div>
+                      </div>
+                      <div className={styles["post-repost"]}>
+                        <div className={styles["post-wrap"]}>
+                          <button className={styles["post-repost-btn"]}></button>
+                          <div className={styles["repost-count"]}>
+                            {post.repostsCount}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div className={styles["post-time"]}>10 days ago</div>
+
+                  {hasDescription && (
+                    <div className={styles["middle-content"]}>
+                      <div className={styles["text-content"]}>{post.description}</div>
+                    </div>
+                  )}
+
+                  {images.length > 0 && (
+                    <div className={styles["post-photos-container"]}>
+                      {images.length > 1 && currentImageIndex > 0 && (
+                        <button
+                          className={`${styles["prl-slider-btn"]} ${styles["prl-left"]}`}
+                          onClick={() =>
+                            changeImage(post.id, "prev", images.length)
+                          }
+                        ></button>
+                      )}
+
+                      <div className={styles["prl-inner-wrapper"]}>
+                        <div className={styles["prl-photo-wrapper"]}>
+                          <img
+                            className={styles["prl-preview-image"]}
+                            src={images[currentImageIndex].mediaUrl}
+                            alt="Post content"
+                            onClick={() =>
+                              openPomhModal(images[currentImageIndex].mediaUrl)
+                            }
+                          />
+                        </div>
+
+                        <div className={styles["prl-dot-container"]}>
+                          {images.map((_, index) => (
+                            <span
+                              key={index}
+                              className={`${styles["prl-dot"]} ${index === currentImageIndex ? styles["prl-active"] : ""
+                                }`}
+                            ></span>
+                          ))}
+                        </div>
+                      </div>
+
+                      {images.length > 1 &&
+                        currentImageIndex < images.length - 1 && (
+                          <button
+                            className={`${styles["prl-slider-btn"]} ${styles["prl-right"]}`}
+                            onClick={() =>
+                              changeImage(post.id, "next", images.length)
+                            }
+                          ></button>
+                        )}
+                    </div>
+                  )}
                 </div>
-                <div className={styles["like-coment-repost"]}>
-                  <div
-                    className={
-                      isLiked(post, user, apiFetch)
-                        ? styles["post-like-btn-active"]
-                        : styles["post-like"]
+              </div>
+
+              <div className={styles["post-comment"]}>
+                <textarea
+                  className={styles["send-comment"]}
+                  placeholder="Write a comment..."
+                  rows={1}
+                  onInput={(e) => {
+                    const target = e.target;
+                    target.style.height = "auto";
+                    target.style.height = Math.min(target.scrollHeight, 100) + "px";
+                  }}
+                  onBlur={(e) => {
+                    const target = e.target;
+                    if (target.value.trim() === "") {
+                      target.style.height = "31px";
                     }
-                    onClick={() => submiteUserLike(post)}
-                  >
-                    <div className={styles["post-wrap"]}>
-                      <button className={styles["post-like-btn"]}></button>
-                      <div className={styles["likes-count"]}>
-                        {post.likesCount}
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className={styles["post-coment"]}>
-                    <div className={styles["post-wrap"]}>
-                      <button className={styles["post-coment-btn"]}></button>
-                      <div className={styles["coment-count"]}>
-                        {post.commentsCount}
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className={styles["post-repost"]}>
-                    <div className={styles["post-wrap"]}>
-                      <button className={styles["post-repost-btn"]}></button>
-                      <div className={styles["repost-count"]}>
-                        {post.repostsCount}
-                      </div>
-                    </div>
-                  </div>
+                  }}
+                />
+                <div className={styles["post-comment-btn"]}>
+                  <button className={styles["publish-btn"]}>Publish</button>
                 </div>
               </div>
 
-              <div className={styles["middle-content"]}>
-                <div className={styles["text-content"]}>{post.description}</div>
-              </div>
-
-
-              {/* <div
+              <div
                 className={styles["see-coments"]}
                 onClick={openDiscussion}
               >
@@ -147,27 +397,11 @@ export default function PostItem({ selectedTab }) {
               onClick={handleShowMore}
             >
               Show more
-            </button> */}
-
-
-              <div className={styles["post-photos-container"]}>
-                {post.contents?.map((content) => (
-                  <div className={styles["post-photo"]} key={content.id}>
-                    <img
-                      className={styles["preview-image"]}
-                      src={content.mediaUrl}
-                      alt="Post content"
-                      onClick={() => openPomhModal(content.mediaUrl)}
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
+            </button>
           </div>
-        </div>
-      ))}
+        )}
+      </div>
 
-      {/* POMH MODAL */}
       {pomhIsModalOpen && (
         <div className={styles["pomh-modal-overlay"]} onClick={closePomhModal}>
           <div
