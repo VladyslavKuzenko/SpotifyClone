70ac2ac288766c4ca28d2d9043ca57b8a277e789
VladyslavKuzenko <dalbayb281@gmail.com>
2025-07-15 20:15:58 +0200

    Refactor CORS and security config, update APIProvider
    Removed CorsGlobalConfig and integrated CORS configuration in SecurityConfig with OAuth2 JWT authentication and stricter request authorization. Also fixed bug, did not working publish publications/stories, everything is working now

--- api/src/main/java/edu/itstep/api/securitySettings/SecurityConfig.java
+++ api/src/main/java/edu/itstep/api/securitySettings/SecurityConfig.java
@@ -1,54 +1,37 @@
-//package edu.itstep.api.securitySettings;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-//
-//import static org.springframework.security.config.Customizer.withDefaults;
-//
-//@Configuration
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .cors(withDefaults())
-//                .csrf(withDefaults())
-//                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-//                .build();
-////        return http
-////                .cors(withDefaults())
-////                .csrf(withDefaults())
-////                .authorizeHttpRequests(auth -> auth
-////                        .requestMatchers("/api/public").permitAll()
-////                        .requestMatchers("/api/private").authenticated()
-////                        .anyRequest().authenticated()
-////                )
-////                .oauth2ResourceServer(oauth2 -> oauth2
-////                        .jwt(withDefaults()) // автоматична валідація токена
-////                )
-////                .build();
-//
-////        return http
-////                .csrf().disable()
-////                .authorizeHttpRequests((authorize) -> authorize
-//////                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
-////                        .anyRequest().permitAll()
-////                )
-//        //           .authorizeHttpRequests((authorize) -> authorize
-////                        .requestMatchers("/api/public").permitAll()
-////                        .requestMatchers("/api/private").authenticated()
-////                        .requestMatchers("/api/private-scoped").hasAuthority("SCOPE_read:messages")
-//        //                 .anyRequest().permitAll()
-//        //       )
-//        //     .cors(withDefaults())
-////                .oauth2ResourceServer(oauth2 -> oauth2
-////                        .jwt(withDefaults())
-////                )
-//        //      .build();
-//    }
-//
-//}
+package edu.itstep.api.securitySettings;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+//                .authorizeHttpRequests((authorize) -> authorize
+////                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+//                        .anyRequest().permitAll()
+//                )
+                .authorizeHttpRequests((authorize) -> authorize
+//                        .requestMatchers("/api/public").permitAll()
+//                        .requestMatchers("/api/private").authenticated()
+//                        .requestMatchers("/api/private-scoped").hasAuthority("SCOPE_read:messages")
+                                .anyRequest().authenticated()
+                )
+                .cors(withDefaults())
+               // .csrf(withDefaults())
+
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(withDefaults())
+                )
+                .build();
+    }
+
+}
