a4c16fe314ee8e126a65a9f07c10f66355577ffa
B1mba <partytopmaker@gmail.com>
2025-07-21 17:01:11 +0300

    меню локації

---
+++ frontend/src/components/main-page/LocationMenu.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect, useRef } from "react";
+import styles from "./main.module.css";
+
+const LocationMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  const locations = [
+    "Kyiv",
+    "Lviv",
+    "Odessa",
+    "Dnipro",
+    "Kharkiv",
+    "Zhytomir",
+        "Zhytomir",
+
+  ];
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSelectLocation = (loc) => {
+    console.log("Selected location:", loc);
+    setIsOpen(false); // Закриваємо меню при виборі
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
+
+  return (
+    <div ref={menuRef} className={styles["aml-location-menu-wrapper"]}>
+      <button className={styles["aml-location-button"]} onClick={toggleMenu}>
+        Add location
+      </button>
+      {isOpen && (
+        <ul className={styles["aml-location-list"]}>
+          {locations.map((loc) => (
+            <li
+              key={loc}
+              className={styles["aml-location-item"]}
+              onClick={() => handleSelectLocation(loc)}
+            >
+              {loc}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default LocationMenu;
